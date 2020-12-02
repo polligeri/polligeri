49f71374caee39b7f6f71c28c58f5293439a4444
Pollák Gergő <polligeri@gmail.com>
2020-12-02 18:54:21 +0100

    Ez lennék én

---
+++ IntroduceYourself.java
@@ -0,0 +1,19 @@
+package Minden.polligeri;
+
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        // Írj egy programot, ami kiír rólad pár részletet a konzolra
+        // Minden egyes részletet új sorba írja a program:
+        //  - Neved
+        //  - Korod
+        //  - Magasságod méterben kifejezve (tört érték)
+        //
+        //  Példa kimenet (output):
+        //  John Doe
+        //  31
+        //  1.87
+        System.out.println("My name: Greg Pollak");
+        System.out.println("My age: 39");
+        System.out.println("My height: " + 1.77);
+    }
+}
