47b1fcae7984a881ac345fd0022e249c3662468b
Pollák Gergő <polligeri@gmail.com>
2020-12-02 20:09:35 +0100

    Idő

---
+++ SecondsInADays.java
@@ -0,0 +1,23 @@
+package Minden.polligeri;
+
+public class SecondsInADays {
+    public static void main(String[] args) {
+        int currentHours = 14;
+        int currentMinutes = 34;
+        int currentSeconds = 42;
+
+        // Írj egy programot, ami kiszámítja, hogy hány másodperc
+        // van még hátra a napból úgy, hogy az aktuális időt a fenti változókkal
+        // lehet beállítani (a végeredmény is legyen egész szám)
+
+        int hátralévőÓra = 24-14;
+        int hátralévőPerc = 60-34;
+        int hátralévőMásodperc = 60-42;
+
+        System.out.print( hátralévőÓra );
+        System.out.print( ":" +hátralévőPerc);
+        System.out.print( ":" +hátralévőMásodperc);
+    }
+}
+    }
+}
