5dcc7710178960739ab422b6286b3060ac150e6f
Pollák Gergő <polligeri@gmail.com>
2020-12-02 19:41:05 +0100

    Változók

---
+++ DefineBasicInfo.java
@@ -0,0 +1,20 @@
+package Minden.polligeri;
+
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        // Hozz létre pár dolgot változóként és utána írasd ki az értékeiket:
+        // - A neved karakterláncként (string)
+        // - Korod egész számként (integer)
+        // - Magasságod méterben törtszámként (float / double)
+        // - Házas vagy-e logikai értékként (boolean)
+        String nev ="Pollák Gergő";
+        int kor = 39;
+        double magassag = 1.77;
+        boolean hazas = true;
+
+        System.out.println(nev);
+        System.out.println(kor);
+        System.out.println(magassag);
+        System.out.println(hazas);
+    }
+}
