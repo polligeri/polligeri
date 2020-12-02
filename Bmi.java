251990eb84d2bca9046669502ac043724b932169
Pollák Gergő <polligeri@gmail.com>
2020-12-02 19:36:39 +0100

    Testtömegindex

---
+++ Bmi.java
@@ -0,0 +1,12 @@
+package Minden.polligeri;
+
+public class Bmi {
+    public static void main(String[] args) {
+        double massInKg = 81.2;
+        double heightInM = 1.78;
+
+        // Számíttasd és írasd ki a testtömeg-indexet (BMI)
+        // a változókat használva
+        System.out.println(massInKg/(heightInM*heightInM));
+    }
+}
