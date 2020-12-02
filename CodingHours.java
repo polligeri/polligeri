7fccb8beef3c8f6047e70e1100d212ed3b1bb257
Pollák Gergő <polligeri@gmail.com>
2020-12-02 19:20:42 +0100

    KódolásÓrák

---
+++ CodingHours.java
@@ -0,0 +1,21 @@
+package Minden.polligeri;
+
+public class CodingHours {
+    public static void main(String[] args) {
+        // Egy átlagos Green Fox hallgató 6 órát kódol naponta
+        // Egy félév hosszúsága 17 hét
+        //
+        // Írd ki, hány órát tölt kódolással egy hallgató egy félév során,
+        // ha a hallgató csak hétköznap kódol
+        //
+        // Írd ki, hogy a félév hány százalékát tölti a hallgató kódolással
+        // ha az átlagos heti munkaidő 52 óra
+
+                double ora = 17 * 6 * 7 - 17 * 2;
+                System.out.println(ora);
+                double munkaIdo = 52 * 17;
+                System.out.println(munkaIdo);
+                double kodOra = (ora / munkaIdo) * 100;
+                System.out.println(kodOra);
+    }
+}
