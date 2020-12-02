062824fe672dba2699e491fd9d88a066cfacd38d
Pollák Gergő <polligeri@gmail.com>
2020-12-02 19:07:35 +0100

    KétSZám

---
+++ TwoNumbers.java
@@ -0,0 +1,26 @@
+package Minden.polligeri;
+
+public class TwoNumbers {
+    public static void main(String[] args) {
+        // Írj egy programot, ami kiírja pár számbéli operáció eredményét 22-vel és 13-mal
+
+        // Írasd ki 13 és 22 összegét
+
+        // Írasd ki 13 és 22 különbségét
+
+        // Írasd ki 13 és 22 szorzatát
+
+        // Írasd ki 22-őt osztva 13-mal művelet eredményét (törtszám)
+
+        // Írasd ki egész számként a 22 osztva 13-mal művelet eredményét
+
+        // Írasd ki 22-őt osztva 13-mal művelet osztási maradékát.
+
+        System.out.println(13+22);
+        System.out.println(13-22);
+        System.out.println(13*22);
+        System.out.println(13/22);
+        System.out.println(22/13);
+        System.out.println(22/13);
+    }
+}
