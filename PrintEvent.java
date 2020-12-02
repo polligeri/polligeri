f6992da1cfea8e55760b5f16d7c07f2cbbaf91b2
Pollák Gergő <polligeri@gmail.com>
2020-12-02 22:18:31 +0100

    Páros0-500

---
+++ PrintEvent.java
@@ -0,0 +1,11 @@
+package Minden.polligeri;
+
+public class PrintEvent {
+    public static void main(String[] args) {
+        // Írj egy programot, ami kiírja az összes páros számot 0 és 500 között
+        for (int i = 0; i < 501; i = i + 2) {
+            System.out.println(i);
+
+        }
+    }
+}
