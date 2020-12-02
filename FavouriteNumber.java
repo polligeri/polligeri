d4fead1f7bf37eb2684332f0179d61533e98b494
Pollák Gergő <polligeri@gmail.com>
2020-12-02 19:26:05 +0100

    KedvencSzámom

---
+++ FavouriteNumber.java
@@ -0,0 +1,11 @@
+package Minden.polligeri;
+
+public class FavouriteNumber {
+    public static void main(String[] args) {
+        int favoriteNumber = 17;
+        // Írd át a fenti számot a kedvenc számoddá
+        // És utána írasd ki efféle képpen: "A kedvenc számom: 8"
+        // Kiíratáskor használd fel a favoriteNumber változót
+        System.out.println("A kedvenc számom: " + favoriteNumber);
+    }
+}
