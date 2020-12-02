2728f040866cf01c7ad562f21bbed007ba461073
Pollák Gergő <polligeri@gmail.com>
2020-12-02 17:07:01 +0100

    Hello

---
+++ Main.java
@@ -0,0 +1,9 @@
+package Minden;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Kiírja a konzolra, hogy "Hello, World!"
+        System.out.println("Hello, World!");
+    }
+}
