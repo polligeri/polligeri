2034e02e74fcebeacfb806e2d3f5f2c2d2c03464
Pollák Gergő <polligeri@gmail.com>
2020-12-02 18:36:22 +0100

    Mások

---
+++ HelloOthers.java
@@ -0,0 +1,14 @@
+package Minden.polligeri;
+
+public class HelloOthers {
+    public static void main(String[] args) {
+        // Üdvözöld 3 osztálytársad 3 sorban kiiratva a nevüket, ehhez hasonlóan:
+        //
+        // Helló Eszter!
+        // Helló Maris!
+        // Helló Józsi!
+        System.out.println("Helló Eszter!");
+        System.out.println("Helló Maris!");
+        System.out.println("Helló Józsi!");
+    }
+}
