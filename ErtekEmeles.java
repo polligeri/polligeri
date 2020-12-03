b85ea1137bdec3238ba147dc4f6a9481b9931a12
Pollák Gergő <polligeri@gmail.com>
2020-12-03 14:34:42 +0100

    AddHundred

---
+++ ErtekEmeles.java
@@ -0,0 +1,9 @@
+package Minden.polligeri;
+
+public class ErtekEmeles{
+    public static void main(String[] args) {
+        int [] numbers = new int[] {1,2,3,4,5,6,};
+        numbers [5] = numbers[5] + 100;
+        System.out.println(numbers[5]);
+    }
+}
