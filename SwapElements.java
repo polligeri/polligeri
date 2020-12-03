f962557362c91d709fe996359e65e41432255b82
Pollák Gergő <polligeri@gmail.com>
2020-12-03 14:52:05 +0100

    swapping elements

---
+++ SwapElements.java
@@ -0,0 +1,20 @@
+package Minden.polligeri;
+
+public class SwapElements {
+    public static void main(String[] args) {
+        // Készíts egy `orders` nevű változót (tömböt)
+        // a következő tartalommal (értékekkel): `[]`
+        // Cseréld fel az első és a harmadik elemet az `orders` tömbben
+
+        String[] orders= new String[]{"first", "second", "third"};
+       /* String tarolo = orders[0];
+        System.out.println(tarolo);
+        orders[0]=orders[2];
+        orders [2]= tarolo;
+        System.out.println(Arrays.toString(orders));*/
+        for (int i = 0; i < orders.length; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
