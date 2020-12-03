83ef86c966b43eed5c207c1620ea7bcf9546216b
Pollák Gergő <polligeri@gmail.com>
2020-12-03 14:32:10 +0100

    Change

---
+++ ElemekValtoztatasa.java
@@ -0,0 +1,9 @@
+package Minden.polligeri;
+
+public class ElemekValtoztatasa{
+    public static void main(String[] args) {
+        int[] numbers = new int[] {1,2,3,8,5,6};
+        numbers[3]=4;
+        System.out.println(numbers[3]);
+    }
+}
