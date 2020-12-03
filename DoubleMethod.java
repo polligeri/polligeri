b28e8d1da7fee7aa3fed1c6efdf73634dc16c50b
Pollák Gergő <polligeri@gmail.com>
2020-12-03 15:30:49 +0100

    duplázza

---
+++ DoubleMethod.java
@@ -0,0 +1,15 @@
+package Minden.polligeri;
+
+public class DoubleMethod {
+
+        public static void main(String[] args) {
+            int baseNum=123;
+            doubling(baseNum);
+            System.out.println(doubling(baseNum));
+        }
+        public static int doubling (int a){
+            return a*2;
+        }
+    }
+
+
