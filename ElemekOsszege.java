34b04a52ec31af73f3fe458de8b1feca1b593c54
Pollák Gergő <polligeri@gmail.com>
2020-12-03 14:28:22 +0100

    AddElement

---
+++ ElemekOsszege.java
@@ -0,0 +1,8 @@
+package Minden.polligeri;
+
+public class ElemekOsszege {
+    public static void main(String[] args) {
+        int [] numbers = new int[] {22,33,44,55};
+        System.out.println(numbers[2] + numbers[3]);
+    }
+}
