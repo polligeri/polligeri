bdedf77e1e1887abcbb9454aa4a8a73396a131ed
Pollák Gergő <polligeri@gmail.com>
2020-12-03 15:53:01 +0100

    dishwasher to galaxy

---
+++ SimpleReplacement.java
@@ -0,0 +1,13 @@
+package Minden.polligeri;
+
+public class SimpleReplacement {
+    public static void main(String[] args) {
+        // Ebben a feladatban szerettem volna a "dishwasher"-t "galaxy"-ra
+        // helyettesíteni, de akadt kis problémám.
+        // Kérlek javítsd meg nekem.
+        // A várt eredmény: In a galaxy far far away
+        String feladat = "In a dishwasher far far away";
+        System.out.println(feladat.replace("dishwasher", "galaxy"));
+    }
+}
+
