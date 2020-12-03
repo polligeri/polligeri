652b918ff8d647f9ab4035a93c87bdcb50f118a0
Pollák Gergő <polligeri@gmail.com>
2020-12-03 14:21:20 +0100

    Harmadik

---
+++ Harmadik.java
@@ -0,0 +1,9 @@
+package Minden.polligeri;
+
+public class Harmadik
+{
+    public static void main(String[] args) {
+        int [] numbers = {4,5,6,7,8};
+        System.out.println("A tömb harmadik eleme : " + numbers[3]);
+    }
+}
